8cb579cb19a31b65fd9623fac737d2e40ced6b08
FIRMAN AHMAD BAYUMI <92419799+FirmanAhmadBayumi@users.noreply.github.com>
2022-09-07 21:08:12 +0700

    Create Input.py

---
+++ Input.py
@@ -0,0 +1,8 @@
+data = input("Type something : ")
+print("Your data is", data, "with type", type(data))
+
+num1 = int(input("Type number decimal : "))
+num2 = float(input("Type number floating : "))
+
+print("Your number is", num1)
+print("Your number is", num2)

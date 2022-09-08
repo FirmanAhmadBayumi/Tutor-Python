f4c3c4b761ef061b9be7e48654d243853526980d
FIRMAN AHMAD BAYUMI <92419799+FirmanAhmadBayumi@users.noreply.github.com>
2022-09-08 22:13:19 +0700

    Update Input.py

--- Input.py
+++ Input.py
@@ -1,8 +1,16 @@
+"""
+  dalam bahasa python ini ketika kita menginput sesuatu maka,
+"""
+
 data = input("Type something : ")
 print("Your data is", data, "with type", type(data))
 
+#statement dibawah input yang diterima adalah bertipe integer
 num1 = int(input("Type number decimal : "))
+
+#statement dibawah input yang diterima adalah bertipe real/float
 num2 = float(input("Type number floating : "))
 
 print("Your number is", num1)
 print("Your number is", num2)
+

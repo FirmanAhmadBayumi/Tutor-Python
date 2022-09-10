43f918f29f4d0e5517fd3f182a123084023661d0
FIRMAN AHMAD BAYUMI <92419799+FirmanAhmadBayumi@users.noreply.github.com>
2022-09-10 21:32:51 +0700

    Update Pangkat(3).py

--- Pangkat(3).py
+++ Pangkat(3).py
@@ -2,3 +2,11 @@ num1, num2, num3 = eval(input("Input 3 buah angka :"))
 
 kuadrat = num1**4, num2**3, num3**2
 
+print("Hasil dari" ,num1, "pangkat 4,", num2, "pangkat 3,", num3, "pangkat 2","adalah",kuadrat)
+
+"""
+                                            CAUTION!!!
+  
+  SAAT MELAKUKAN PROSES INPUT WAJIB MENYISIPKAN TANDA "," SETELAH MEMBERIKAN VALUE DI SETIAP VARIABEL!!!!!!!
+  
+"""

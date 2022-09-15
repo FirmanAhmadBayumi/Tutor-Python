88008a47544f93111cc1cc28d738dfba1290b5a0
FIRMAN AHMAD BAYUMI <92419799+FirmanAhmadBayumi@users.noreply.github.com>
2022-09-15 20:52:05 +0700

    Update Latihan_Perkalian.py

--- Latihan_Perkalian.py
+++ Latihan_Perkalian.py
@@ -1,7 +1,8 @@
 num1 = int(input("Masukkan Angka-1: ")) 
 num2 = int(input("Masukkan Angka Ke-2: ")) 
 num3 = int(input("Masukkan angka ke-3: "))
+num4 = int(input("Masukkan angka ke-4: ")) 
 
-res = num1 * num2 / num3
+res = num1 * num2 / num3 + num4
 
-print("Hasil dari", num1, "x", num2, "/", num3, "adalah", res) 
+print("Hasil dari", num1, "x", num2, "/", num3, "+", num4, "adalah", res) 
